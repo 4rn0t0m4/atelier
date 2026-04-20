de658f6ec6039ed3f1f5746c828e80df08d3f958
Arnaud Thomas <arnaud.thomas@normandiepharma.fr>
2026-04-20 17:34:16 +0200

    fix: redirection 301 de new.atelier-aubin.fr vers www

---
+++ app/Http/Middleware/RedirectLegacyHost.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RedirectLegacyHost
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $host = $request->getHost();
+
+        if (str_starts_with($host, 'new.') || str_starts_with($host, 'www.new.')) {
+            $target = 'https://www.atelier-aubin.fr' . $request->getRequestUri();
+
+            return redirect($target, 301);
+        }
+
+        return $next($request);
+    }
+}
